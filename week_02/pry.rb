e32a73a5dc4e475b7468105737c7773278ddb198
Dianna Tingg <dtingg@gmail.com>
2019-08-12 16:36:35 -0700

    added pry

---
+++ week_02/pry.rb
@@ -0,0 +1,18 @@
+# Ada - Week 02
+# Gems Jigsaw
+
+require "pry"
+
+puts "What is your name?"
+name = gets.chomp
+
+binding.pry
+
+first_num = 4
+second_num = 8
+
+binding.pry
+
+first_num = 10
+
+binding.pry
