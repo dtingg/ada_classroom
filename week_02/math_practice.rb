ef289febe710123de2e838ae54357bc9bbf91a77
Dianna Tingg <dtingg@gmail.com>
2019-08-13 20:15:04 -0700

    added math_practice

---
+++ week_02/math_practice.rb
@@ -0,0 +1,136 @@
+# Essential Mathematics - practice problems
+# CS fundamentals 0
+
+# Sorted items
+# For each of the following list of objects, share whether the objects are: sorted in ascending order, sorted in descending order, unsorted.
+
+# 1. [2, 4, 6, 8, 10]
+# sorted in ascending order
+
+# 2. [-123, -42, -21, -9, 150, 2100]
+# sorted in ascending order
+
+# 3. ["oranges", "kiwis", bananas", "apples"]
+# sorted in descending order
+
+# 4. [-3, 705, "apples", 3.14]
+# unsorted
+
+# 5. [32, 64, 16, 8, 128]
+# unsorted
+
+# 6. [320, 230, 30, 20, 0]
+# sorted in descending order
+
+# Maximum and Minimum
+# For each of the following list of numbers, what is the maximum value and minimum value?
+
+# 1. [91, 42, 128, 47, 256, 19, 9, 254]
+# max = 256
+# min = 9
+
+# 2. [14, 19, 17, 21, 15, 18]
+# max = 21
+# min = 14
+
+# Mean and Median
+# What is the mean and median for each of the following list of numbers?
+
+# 1. [5, 3, 1, 2, 9]
+# mean = 4
+# median = 3
+
+# 2. [19, 21, 14]
+# mean = 18
+# median = 19
+
+# 3. [35, 30, 25, 10, 15, 20, 5]
+# mean = 20
+# median = 20
+
+# 4. [3, 1, 2]
+# mean = 2
+# median = 2 
+
+# 5. [15, 4, 3, 2, 1, 9, 8, 7, 5]
+# mean = 6
+# median = 5
+
+# Modulo and Divide
+# Considering integer division, for each of the following what would be the result?
+
+# 1. 21 / 10
+# 2
+
+# 2. 21 % 10
+# 1
+
+# 3. 1234 / 100
+# 12
+
+# 4. 1234 % 100
+# 34
+
+# 5. 547210 / 10000
+# 54
+
+# 6. 547210 % 10000
+# 7210
+
+# 7. 65 / 2
+# 32
+
+# 8. 65 % 2
+# 1
+
+# Exponents and Logarithms
+# Solve the following problems related to exponents and logarithms.
+
+# 1. 10^5
+# 100000
+
+# 2. 2^2
+# 4
+
+# 3. 2^4
+# 16
+
+# 4. 789^0
+# 1
+
+# 5. 2^5
+# 32
+
+# 6. 2^8
+# 256
+
+# 7. log2 (256)
+# 8
+
+# 8. log2 (64)
+# 6
+
+# 9. log2 (8)
+# 3
+
+# 10. log2 (1)
+# 0
+
+# Set theory basics
+# What is the intersection and union of the following two sets?
+
+# 1. Set 1: {}, Set 2: {34, 73, 21}
+# intersection: {}
+# union: {34, 73, 21}
+
+# 2. Set 1: {61, 21, 19, 18}, Set 2: {78, 12, 16, 21}
+# intersection: {21}
+# union: {61, 21, 19, 18, 78, 12, 16}
+
+# 3. Set 1: {21, 12, 34, 43, 73, 37}, Set 2: {12, 21, 34, 37, 43, 73, 91}
+# intersection:{12, 21, 34, 37, 43, 73}
+# union: {12, 21, 34, 37, 43, 73, 91}
+
+# 4. Set 1: {2, 4, 6, 8, 10, 12}, Set 2: {3, 6, 9, 12, 15}
+# intersection:{6, 12}
+# union: {2, 3, 4, 6, 8, 9, 12, 15}
