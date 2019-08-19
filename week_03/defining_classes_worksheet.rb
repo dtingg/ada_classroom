b45281a04f56d5bc4c50436b4580ca227898b0db
Dianna Tingg <dtingg@gmail.com>
2019-08-19 12:34:51 -0700

    added defining classes workshet

---
+++ week_03/defining_classes_worksheet.rb
@@ -0,0 +1,39 @@
+# Ada - Week 03
+# Defining Classes Worksheet
+
+# Superhero
+# Define a class that represents the state and behavior of a superhero, named Superhero by following these steps.
+
+# Celsius
+# Define a class that represents the state and behavior of the temperature unit Celsius, named Celsius.
+# Every instance of Celsius should be instantiated with temperature. What syntax do we use to define that? def initialize(temperature)
+# Every instance of Celsius should keep track of its own temperature. What syntax do we use to define that, and assign it to the temperature we passed in? @temperature = temperature
+# Every instance of Celsius has the behavior of returning its temperature as Fahrenheit. What syntax do we use to define behavior? def fahrenheit
+
+# Optional: CoffeeOrder
+# Define a class called CoffeeOrder that has a lot of parameters. Maintain the practice of using main.rb to catch your syntax errors and practice making and using instances of classes.
+
+# Optional: ProfessionalYeller
+# Prove to yourself that values of instance variables can be modified within an instance of a class, but instance variables between different instances do not interact.
+# Does a ProfessionalYeller take in any arguments whenever a new instance is made? - favorite_phrase
+# What does a ProfessionalYeller keep track of as pieces of state (as instance variables)? - phrase, count
+# What does the behavior (instance method) yell do? - It returns the phrase count number of times and increases count by 1.
+# What should we see in the Terminal when we run main.rb?
+
+# A
+# LOVE
+# PEACE
+# LOVELOVE
+# PEACEPEACE
+# LOVELOVELOVE
+# PEACEPEACEPEACE
+# PEACEPEACEPEACEPEACE
+# PEACEPEACEPEACEPEACEPEACE
+# LOVELOVELOVELOVE
+# PEACEPEACEPEACEPEACEPEACEPEACE
+# LOVELOVELOVELOVELOVE
+# AA
+# AAA
+# AAAA
+# AAAAA
+# AAAAAA
