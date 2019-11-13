22a59fdcc023c7902f131535f91c8650f55f53cb
Dianna Tingg <dtingg@gmail.com>
2019-11-13 09:43:17 -0800

    added arrow functions

---
+++ week_15/arrow-functions.js
@@ -0,0 +1,12 @@
+const foo = () => 'bar';
+
+console.log(foo());
+// bar
+
+// const hello = function hello() {
+//   return 'hello world';
+// };
+
+const hello = () => 'hello world';
+
+console.log(hello())
