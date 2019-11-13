1103b603fcc631a17d577933d1abc67bdae748c2
Dianna Tingg <dtingg@gmail.com>
2019-11-12 16:10:00 -0800

    adding calculator

---
+++ week_15/js-calculator/calculator.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const addNums = function(a, b) {
+  return a + b
+}
+
+const subtractNums = function(a, b) {
+  return a - b
+}
+
+const multiplyNums = function(a, b) {
+  return a * b
+}
+
+const divideNums = function(a, b) {
+  return a / b
+}
+
+const calculator = function(error, result) {
+  let num1 = parseInt(result.num1)
+  let num2 = parseInt(result.num2)
+  let operation = result.operation
+
+  let answer;
+
+  switch(operation) {
+    case '+':
+      answer = addNums(num1, num2);
+      break;
+    case "-":
+      answer = subtractNums(num1, num2);
+      break;
+    case "*":
+      answer = multiplyNums(num1, num2);
+      break;
+    case "/":
+      answer = divideNums(num1, num2);
+      break;
+    default:
+      answer = `I don't know what ${operation} means.`;
+      break;
+  }
+  console.log(answer);
+} 
+
+const prompt = require('prompt');
+
+//start the prompt
+prompt.start();
+
+//collect two numbers (num1 and num2) and an operation
+//then call the function `calculator` with the results
+prompt.get(['num1','num2','operation'], calculator);
