439eb0c7d0a7bce757d01a73b79d3d9bfe55731b
Dianna Tingg <dtingg@gmail.com>
2019-11-22 11:07:04 -0800

    added styles

--- week_16/hello-world/src/components/Student.js
+++ week_16/hello-world/src/components/Student.js
@@ -3,16 +3,29 @@ import "./Student.css";
 import PropTypes from "prop-types";
 
 const Student = (props) => {
+  const buildStyles = () => {
+    let selectStyles = props.class ? props.class.toLowerCase() : "";
+    selectStyles = selectStyles + " " + (props.present ? "present" : "absent");
+    return selectStyles
+  }
+
   return(
-    <h3>{props.fullName} {props.email} {props.class} {props.present} </h3>
+    <li className={buildStyles()}>
+      {props.fullName} <p>{props.email}</p>{props.class} {props.present}
+    </li>
   );
+}
+
+Student.defaultProps = {
+  class: "no-class",
+  email: "No Email On File"
 }
 
 Student.propTypes = {
   fullName: PropTypes.string.isRequired,
   email: PropTypes.string,
   class: PropTypes.string,
-  present: PropTypes.bool,
+  present: PropTypes.bool.isRequired,
 };
 
 export default Student;
