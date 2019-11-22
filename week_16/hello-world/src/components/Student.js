383fb2989d1e7bf5196ff5ac38761092d241f1c5
Dianna Tingg <dtingg@gmail.com>
2019-11-22 10:52:42 -0800

    updated prop types

--- week_16/hello-world/src/components/Student.js
+++ week_16/hello-world/src/components/Student.js
@@ -4,13 +4,15 @@ import PropTypes from "prop-types";
 
 const Student = (props) => {
   return(
-    <h3>{props.fullName} {props.email}</h3>
+    <h3>{props.fullName} {props.email} {props.class} {props.present} </h3>
   );
 }
 
 Student.propTypes = {
   fullName: PropTypes.string.isRequired,
   email: PropTypes.string,
+  class: PropTypes.string,
+  present: PropTypes.bool,
 };
 
 export default Student;
--- week_16/hello-world/src/components/StudentCollection.js
+++ week_16/hello-world/src/components/StudentCollection.js
@@ -7,17 +7,21 @@ const StudentCollection = () => {
     {
       fullName: "Ada Lovelace",
       email: "ada@lovelace.uk",
+      class: "Leaves",
+      present: false,
     },
     {
       fullName: "Katherine Johnson",
       email: "kat@nasa.gov",
+      class: "Branches",
+      present: true,
     },
   ];
 
   const studentComponents = students.map((student, i) => {
     return (
       <li key={i}>
-        <Student fullName={ student.fullName } email={ student.email } />
+        <Student fullName={ student.fullName } email={ student.email } class={ student.class } present={ student.present } />
       </li>
     );
   });
