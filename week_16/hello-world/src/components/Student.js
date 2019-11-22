f7a0e861b0d1bd3e8e9e1d9f3855e85c3e76e675
Dianna Tingg <dtingg@gmail.com>
2019-11-22 10:36:15 -0800

    added prop types

--- week_16/hello-world/src/components/Student.js
+++ week_16/hello-world/src/components/Student.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import "./Student.css";
+import PropTypes from "prop-types";
 
 const Student = (props) => {
   return(
     <h3>{props.fullName} {props.email}</h3>
   );
 }
+
+Student.propTypes = {
+  fullName: PropTypes.string.isRequired,
+  email: PropTypes.string,
+};
 
 export default Student;
